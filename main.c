03426c2663918c323b5654c85563f8ae71607b01
Ivan Batista <ivanbatista@gmail.com>
2022-11-22 23:02:48 -0300

    Add login
    Co-authored-by: Breno Silva <brenophp@gmail.com>

--- main.c
+++ main.c
@@ -3,6 +3,30 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    FILE *base;
+    char user[10];
+    char senha[10];
+    char user1[10];
+    char senha1[10];
+
+    printf("BEM-VINDO AO C-19\n");
+    printf("Usuario: ");
+    fflush(stdin);
+    gets(user);
+    
+    printf("Senha: ");
+    fflush(stdin);
+    gets(senha);
+    
+    base = fopen ("login.txt", "r");
+ 
+    while (fscanf(base, "%s %s", &user1, &senha1) != EOF){
+        printf("\nComparando %s:%s com %s:%s",user1,senha1,user,senha);
+        if ((strcmp(user1,user) == 0) && (strcmp(senha1,senha) == 0 )){
+            printf("\nSucesso!!!");
+            break;
+        }
+    }
+
     return 0;
 }
